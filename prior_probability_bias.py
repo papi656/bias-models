19a98e8b1db412dcefc48fae85766d46c181709e
Abhishek Pratap Singh <abhishekprataps656@gmail.com>
2023-05-29 12:01:58 +0530

    changes for reading .txt file

--- prior_probability_bias.py
+++ prior_probability_bias.py
@@ -1,62 +1,54 @@
 import os
-import pandas as pd
-import csv
 import argparse
-from tqdm import tqdm
 
 
 input_path = 'datasets'
 output_path = 'resources'
 
 def read_and_count(dataset_name):
-    path = os.path.join(input_path, dataset_name, 'train.tsv')
+    file_path = os.path.join(input_path, dataset_name, 'train.txt')
     tag_counts = {}
-    data = pd.read_csv(path, sep='\t', quoting=csv.QUOTE_NONE, names=['Tokens', 'Labels'], skip_blank_lines=False)
-
-    data = data.dropna()
-
-    for i in range(data.shape[0]):
-        token, label = data.iloc[i].values
-        if token.lower() not in tag_counts:
-            tag_counts[token.lower()] = {'B':0, 'I':0, 'O': 0}
-            tag_counts[token.lower()][label] += 1
-        else:
-            tag_counts[token.lower()][label] += 1
-
-    
-    return tag_counts
-
-def generate_probability_dist_for_tokens(dataset_name, tag_counts, output_file):
-    i_path = os.path.join(input_path, dataset_name, 'train.tsv')
-    data = pd.read_csv(i_path, sep='\t', quoting=csv.QUOTE_NONE, names=['Tokens', 'Labels'], skip_blank_lines=False)
-
-    o_path = os.path.join(output_path, dataset_name, output_file)
-    with open(o_path, 'w') as fh:
-        for i in tqdm(range(data.shape[0])):
-            token, label = data.iloc[i].values
-            if isinstance(token, float): # means it is empty line
-                fh.write('\n')
+    token_lst = []
+    with open(file_path, 'r') as fh:
+        for line in fh:
+            if len(line.strip()) == 0:
+                token_lst.append("")
+                continue 
+            parts = line.split('\t')
+            token, label = parts[0].strip(), parts[1].strip()
+            token_lst.append(token)
+            if token.lower() not in tag_counts:
+                tag_counts[token.lower()] = {'B':0, 'I':0, 'O':0}
+                tag_counts[token.lower()][label] += 1
             else:
-                b_cnt = tag_counts[token.lower()]['B']
-                i_cnt = tag_counts[token.lower()]['I']
-                o_cnt = tag_counts[token.lower()]['O']
-                total_cnt = b_cnt + i_cnt + o_cnt
-                fh.write(f'{b_cnt/total_cnt},{i_cnt/total_cnt},{o_cnt/total_cnt}\n')
+                tag_counts[token.lower()][label] += 1
 
+    return tag_counts, token_lst
+
+def generate_probability_dist_for_tokens(dataset_name, tag_counts, token_lst):
+
+    output_file_path = os.path.join(output_path, dataset_name, 'BC5CDR_prior_prob.txt')
+    with open(output_file_path, 'w') as fh:
+        for tok in token_lst:
+            if len(tok.strip()) == 0:
+                fh.write(f'\n')
+            else:
+                b_cnt = tag_counts[tok.lower()]['B']
+                i_cnt = tag_counts[tok.lower()]['I']
+                o_cnt = tag_counts[tok.lower()]['O']
+                total_cnt = b_cnt + i_cnt + o_cnt 
+                fh.write(f'{b_cnt/total_cnt},{i_cnt/total_cnt},{o_cnt/total_cnt}\n')
 
 
 def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--dataset_name', type=str, required=True)
-    parser.add_argument('--output_file', type=str, required=True)
 
     args = parser.parse_args()
 
-    tag_counts = read_and_count(args.dataset_name)
-    generate_probability_dist_for_tokens(args.dataset_name, tag_counts, args.output_file)
-
+    tag_counts, token_lst = read_and_count(args.dataset_name)
+    generate_probability_dist_for_tokens(args.dataset_name, tag_counts, token_lst)
 
 if __name__ == '__main__':
     main()
-
